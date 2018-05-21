27e1ce16ecb6ecf18f0ace5143fb74a09e48d0fc
alexBSommer <alex.b.sommer@gmail.com>
2018-05-21 11:45:09 -0400

    Partial Authentication

---
+++ ServiceLayer/ServiceLayer/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using ServiceLayer.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Owin.Security;
+using System.Security.Claims;
+
+namespace ServiceLayer.Controllers
+{
+    public class AccountController : ApiController
+    {
+        /*
+        [HttpPost]
+        [Route]
+        [AllowAnonymous]
+        public IHttpActionResult Register(AccountModel account)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            //var userStore = new UserStore<IdentityUser>(new DataDbContext());
+           // var userManager = new UserManager<IdentityUser>(userStore);
+            var user = new IdentityUser(account.Email);
+
+            if (userManager.Users.Any(u => u.UserName == account.Email))
+            {
+                return BadRequest();
+            }
+
+            userManager.Create(user, account.Pwd);
+
+            return Ok();
+        }
+        */
+    }
+}
---
+++ ServiceLayer/ServiceLayer/Models/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+ namespace 
+public class Class1
+{
+	public Class1()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}

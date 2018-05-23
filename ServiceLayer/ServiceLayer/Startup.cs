a73fb5b0a91747f0486286a708f0744465710aa0
alexBSommer <alex.b.sommer@gmail.com>
2018-05-23 16:48:22 -0400

    Cookies reference and Startup.cs

---
+++ ServiceLayer/ServiceLayer/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.Cookies;
+
+[assembly: OwinStartup(typeof(ServiceLayer.Startup))]
+namespace ServiceLayer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.UseCookieAuthentication(new CookieAuthenticationOptions()
+            {
+                AuthenticationType = WebApiConfig.AuthenticationType,
+                CookieName = WebApiConfig.CookieName
+            });
+        }
+    }
+}

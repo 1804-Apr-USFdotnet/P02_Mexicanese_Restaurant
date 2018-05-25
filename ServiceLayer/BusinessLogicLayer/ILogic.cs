45eb5767ea1e8bc05443363e85b4a3467525c17f
Steven Nagy <geiganax@gmail.com>
2018-05-25 10:00:53 -0400

    admin role added

--- ServiceLayer/BusinessLogicLayer/ILogic.cs
+++ ServiceLayer/BusinessLogicLayer/ILogic.cs
@@ -5,6 +5,7 @@ namespace BusinessLogicLayer
     public interface ILogic
     {
         void Register(LogicIdentityModel account);
+        void RegisterAdmin(LogicIdentityModel account);
         void Login(LogicIdentityModel account);
     }
 }
--- ServiceLayer/BusinessLogicLayer/identityLogic.cs
+++ ServiceLayer/BusinessLogicLayer/identityLogic.cs
@@ -29,5 +29,21 @@ namespace BusinessLogicLayer
         {
             throw new NotImplementedException();
         }
+
+        public void RegisterAdmin(LogicIdentityModel account)
+        {
+            // actually register
+            var userStore = new UserStore<IdentityUser>(new IDDBContext());
+            var userManager = new UserManager<IdentityUser>(userStore);
+            var user = new IdentityUser(account.UserName);
+
+            if (userManager.Users.Any(u => u.UserName == account.UserName))
+            { throw new Exception(); }
+
+            userManager.Create(user, account.Password);
+
+            // the only difference from Register action
+            userManager.AddClaim(user.Id, new Claim(ClaimTypes.Role, "admin")); 
+        }
     }
 }

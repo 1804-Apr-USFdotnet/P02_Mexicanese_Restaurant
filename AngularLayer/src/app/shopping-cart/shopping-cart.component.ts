4b56f7ac5fe3db86bebc5cfecd241aba4abea9e4
Steven Nagy <geiganax@gmail.com>
2018-05-29 06:23:28 -0400

    more angular

--- AngularLayer/src/app/shopping-cart/shopping-cart.component.ts
+++ AngularLayer/src/app/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Input,Component, OnInit } from '@angular/core';
+import {menuItem} from '../models/menuItem';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-cart.component.css']
 })
 export class ShoppingCartComponent implements OnInit {
+
+  public items: menuItem = {itemID:1,itemName:'name',itemDescription:'testdesc',itemPrice:1.00,Stock:2};
 
   constructor() { }
 

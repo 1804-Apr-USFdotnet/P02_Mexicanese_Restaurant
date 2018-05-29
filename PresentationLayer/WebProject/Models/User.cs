4234e7f45798ae0b7f53ce11f7c125a927538968
Djtooth <davidjnelson17@gmail.com>
2018-05-28 23:28:26 -0400

    Saving Working state

--- PresentationLayer/WebProject/Models/User.cs
+++ PresentationLayer/WebProject/Models/User.cs
@@ -13,7 +13,6 @@ namespace WebProject.Models
         [StringLength(255)]
         public string Pwd { get; set; }
 
-
         public int AccessLevel { get; set; }
 
         //[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
